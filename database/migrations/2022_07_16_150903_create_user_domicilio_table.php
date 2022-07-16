2303f17b8b62c3c47e47982cc5cd61a3141ee7b8
RaulMartinez <murillo25@hotmail.es>
2022-07-16 10:17:23 -0500

    Migracion tabla user_domicilio

---
+++ database/migrations/2022_07_16_150903_create_user_domicilio_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->integer('numero_exterior');
+            $table->string('colonia');
+            $table->integer('cp');
+            $table->string('ciudad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+};
